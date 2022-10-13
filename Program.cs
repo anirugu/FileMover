402151c606a8852cae50db3495c3b6223fb234a7
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-10-13 14:13:24 +0530

    tested the code, not working

--- Program.cs
+++ Program.cs
@@ -9,6 +9,19 @@ namespace FileMover
             var settingsFile = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "settings.json");
             string text = System.IO.File.ReadAllText(settingsFile);
             Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(text);
+
+            var directories = System.IO.Directory.GetDirectories(obj.source);
+            foreach (var dir in directories)
+            {
+                bool excluded = obj.exclude.Any(x => dir.EndsWith(x));
+                if (!excluded)
+                {
+                    var name = System.IO.Path.GetFileName(dir);
+                    string newPath = System.IO.Path.Combine(obj.target, name);
+                    System.IO.Directory.Move(dir, newPath);
+                }
+            }
+
             Console.WriteLine("Hello, World!");
         }
     }
