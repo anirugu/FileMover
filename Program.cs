4abfd47136466aec6fd5965f802d612dd41f35ed
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-10-13 13:54:47 +0530

    saving changes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace FileMover
+﻿using Newtonsoft.Json;
+
+namespace FileMover
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var settingsFile = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "settings.json");
+            string text = System.IO.File.ReadAllText(settingsFile);
+            Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(text);
             Console.WriteLine("Hello, World!");
         }
     }
+}
+
+
+public class Rootobject
+{
+    public string source { get; set; }
+    public string target { get; set; }
+    public string[] exclude { get; set; }
 }
